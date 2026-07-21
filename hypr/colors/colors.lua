90a29397f99e66cd8bf4115f04d2c12e12025fe6
nathanWorkout <delahayenathan313@gmail.com>
2026-07-21 09:02:36 +0200

    mes dotfiles

---
+++ hypr/colors/colors.lua
@@ -0,0 +1,104 @@
+return {
+    image = "/home/nathan/Pictures/Rice/c++.png",
+
+    background = "0xff18120b",
+
+    error = "0xffffb4ab",
+
+    error_container = "0xff93000a",
+
+    inverse_on_surface = "0xff362f27",
+
+    inverse_primary = "0xff805610",
+
+    inverse_surface = "0xffede0d4",
+
+    on_background = "0xffede0d4",
+
+    on_error = "0xff690005",
+
+    on_error_container = "0xffffdad6",
+
+    on_primary = "0xff452b00",
+
+    on_primary_container = "0xffffddb4",
+
+    on_primary_fixed = "0xff291800",
+
+    on_primary_fixed_variant = "0xff633f00",
+
+    on_secondary = "0xff3e2d16",
+
+    on_secondary_container = "0xfffbdebc",
+
+    on_secondary_fixed = "0xff271904",
+
+    on_secondary_fixed_variant = "0xff56432b",
+
+    on_surface = "0xffede0d4",
+
+    on_surface_variant = "0xffd3c4b4",
+
+    on_tertiary = "0xff253515",
+
+    on_tertiary_container = "0xffd4eabb",
+
+    on_tertiary_fixed = "0xff101f03",
+
+    on_tertiary_fixed_variant = "0xff3b4c29",
+
+    outline = "0xff9c8f80",
+
+    outline_variant = "0xff4f4539",
+
+    primary = "0xfff5bc6f",
+
+    primary_container = "0xff633f00",
+
+    primary_fixed = "0xffffddb4",
+
+    primary_fixed_dim = "0xfff5bc6f",
+
+    scrim = "0xff000000",
+
+    secondary = "0xffddc2a1",
+
+    secondary_container = "0xff56432b",
+
+    secondary_fixed = "0xfffbdebc",
+
+    secondary_fixed_dim = "0xffddc2a1",
+
+    shadow = "0xff000000",
+
+    source_color = "0xff6e5128",
+
+    surface = "0xff18120b",
+
+    surface_bright = "0xff3f3830",
+
+    surface_container = "0xff251f17",
+
+    surface_container_high = "0xff302921",
+
+    surface_container_highest = "0xff3b342b",
+
+    surface_container_low = "0xff211b13",
+
+    surface_container_lowest = "0xff130d07",
+
+    surface_dim = "0xff18120b",
+
+    surface_tint = "0xfff5bc6f",
+
+    surface_variant = "0xff4f4539",
+
+    tertiary = "0xffb9cda0",
+
+    tertiary_container = "0xff3b4c29",
+
+    tertiary_fixed = "0xffd4eabb",
+
+    tertiary_fixed_dim = "0xffb9cda0",
+
+}
